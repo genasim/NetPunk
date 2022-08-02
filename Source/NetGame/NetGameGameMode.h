e0577aec859465d6fa4db51338310bc83dd1274e
Genadi-Kolev <93799385+Genadi-Kolev@users.noreply.github.com>
2022-08-02 12:08:28 +0300

    Initial Commit

---
+++ Source/NetGame/NetGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANetGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetGameGameMode();
+};
+
+
+
